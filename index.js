28f25af82ab595b6ffbd6d4903ba489ab4cdd84c
Sumbat Gozalov <gozalovsumbat@yahoo.com>
2023-07-16 20:49:40 +0400

    end of task2

---
+++ index.js
@@ -0,0 +1,33 @@
+//          Task 1 sum of only positive
+// let sumofposnum = [1, 2, 5, -2, -1, 0, 4, 3, -1];
+
+// let sum = 0;
+
+// for (let i = 0; i < sumofposnum.length; i++) {
+//   if (sumofposnum[i] > 0) {
+//     sum += sumofposnum[i];
+//   }
+// }
+
+// console.log(sum)
+
+
+//            Task 2 display only evens
+
+// let numbersArray = [1, 2, 5, -2, -1, 0, 4, 3, -1];
+
+
+
+// for (let i = 0; i < numbersArray.length; i++) {
+//     const evennum=numbersArray[i];
+//   if (evennum %2===0 ) {
+//     console.log(evennum)
+//   }
+// }
+
+
+
+
+
+
+
